704abecdc965fbf065b7a2fbab4e64680b4bf040
Manan Kansara <mananpk2002@gmail.com>
2024-08-24 23:07:56 +0530

    Fixed husky configurations

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import * as mongoose from 'mongoose';
 import router from './routes';
-import * as config  from './config/config';
+import * as config from './config/config';
 import logger from './config/logger';
 
 const app = express();
@@ -17,15 +17,15 @@ app.use((req: Request, res: Response) => {
 });
 
 // if we remove next then express can not recognize it as error handling
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+app.use((err: Error, req: Request, res: Response) => {
   if (err) {
     logger.error(err.message);
     res.status(500).send(err.message);
   }
-}); 
+});
 
 mongoose.connect(config.connectionUrl).then(() => {
-  logger.info("Connected to database")
+  logger.info('Connected to database');
   app.listen(() => {
     logger.info(`Server is listening on port ${PORT}`);
   });
